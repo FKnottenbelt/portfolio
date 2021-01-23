1d0043eea857ee828b05ec20b574596047498355
Femke Knottenbelt <femknotgithub@gmail.com>
2021-01-23 15:46:58 +0100

    Add new and create actions for gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,5 +4,19 @@ class GalleriesController < ApplicationController
     @gallery_items = Gallery.all
   end
 
+  def new
+    @gallery_item = Gallery.new
+  end
 
+  def create
+    @gallery_item = Gallery.new(params.require(:gallery).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @gallery_item.save
+        format.html { redirect_to galleries_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end 
+  end
 end
