2ca09be5a0c65b73fcb472b49dd69d19b11f4abc
Femke Knottenbelt <femknotgithub@gmail.com>
2021-09-28 08:47:08 +0200

    Add tryout code for copyright gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = FkViewTool::Renderer.copyright 'F.Knottenbelt', 'All rights reserved'
+  end
+end
+
+module FkViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.new.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
